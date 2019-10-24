6ed3f3a93633509dc1b2d9543c9ec82358c21e71
SoyStillGood <zachzemokhol@gmail.com>
2019-10-24 16:55:59 -0400

    Advanced

---
+++ FileReader.py
@@ -0,0 +1,8 @@
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument("-f", "--file", dest="file", help="input housing data file")
+
+args = parser.parse_args()
+file = args.file
+data=open(file)
+my_file = data.read()

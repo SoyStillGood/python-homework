d9c9f892bd96a6372d2a25ca34520f803154bd24
SoyStillGood <zachzemokhol@gmail.com>
2019-10-24 16:52:15 -0400

    Basic, Completed

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -3,4 +3,18 @@ my_list = range(101)
 #print(list(my_list[1::]))
 
 my_ints = list(my_list[1::])
-#print(my_ints)
+print(my_ints)
+
+def fizz_buzz(num):
+    if num%3==0 and num%5==0:
+        return 'FizzBuzz'
+    elif num%5==0:
+        return 'Buzz'
+    elif num%3==0:
+        return 'Fizz'
+    else:
+        return num
+
+for num in my_ints:
+    print(fizz_buzz(num))
+

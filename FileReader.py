be5566ab637acf13818d85f095a8c9a1983797ad
SoyStillGood <zachzemokhol@gmail.com>
2019-10-24 17:02:28 -0400

    Advanced2

--- FileReader.py
+++ FileReader.py
@@ -5,4 +5,4 @@ parser.add_argument("-f", "--file", dest="file", help="input housing data file")
 args = parser.parse_args()
 file = args.file
 data=open(file)
-my_file = data.read()
+my_file = data.readlines()

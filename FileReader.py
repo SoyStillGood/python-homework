a164f99a5cae3c555d74ab86336a692148ce0361
SoyStillGood <zachzemokhol@gmail.com>
2019-10-24 17:10:42 -0400

    Advanced5

--- FileReader.py
+++ FileReader.py
@@ -5,7 +5,7 @@ parser.add_argument("-f", "--file", dest="file", help="input housing data file")
 args = parser.parse_args()
 file = args.file
 data=open(file)
-my_file = data.readlines()
+my_file = data.read()
 
 my_file_lines = my_file.split('\n')
 print(my_file)

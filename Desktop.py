761df302209d73817331aa8e1887cbef972c2fcb
SoyStillGood <zachzemokhol@gmail.com>
2019-10-25 20:57:40 -0400

    Reach Part 2 extended 4

--- Desktop.py
+++ Desktop.py
@@ -3,8 +3,11 @@ path = "C:/Users/zachz/Desktop"
 dirs = os.listdir(path)
 print(dirs)
 
+IsFile = []
 for file in dirs:
     if os.path.isfile(file)==True:
-        print(file, 'file')
+        IsFile.append(file)
     else:
         print(file, 'directory')
+
+print(IsFile)
